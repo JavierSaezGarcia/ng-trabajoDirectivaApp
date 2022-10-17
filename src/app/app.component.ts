be2840d169a091bffe7ad6d577d312714de5e443
Javier Sáez García <javiersaez1205@gmail.com>
2022-10-17 21:12:11 +0200

    tercer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,22 @@ export class AppComponent {
   registrado = false;
   nombre:string = "";
   apellido:string = "";
+  entradas: any[];
+
   registrarUsuario(){    
     this.registrado = true;
     this.mensaje="Usuario registrado con éxito";   
+  }
+  constructor(){
+    this.entradas=[
+      {titulo:"Python cada día más presente"},
+      {titulo:"Java presnete desde hace 20 años"},
+      {titulo:"Javascript refuerza"},
+      {titulo:"Kotlin potencia tus apps"},
+      {titulo:"Donde esta pascal?"}
+
+    ];
+
   }
   
 }
